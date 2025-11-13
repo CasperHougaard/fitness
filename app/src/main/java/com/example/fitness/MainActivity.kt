b7bf127b252bed08e5198b3ac802b1eb021898ea
Casper Hougaard <casper.hougaard@strusoft.com>
2025-11-13 05:55:43 +0100

    test

--- app/src/main/java/com/example/fitness/MainActivity.kt
+++ app/src/main/java/com/example/fitness/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.fitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.example.fitness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +13,5 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
-    }
-
-    /**
-     * A native method that is implemented by the 'fitness' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-        // Used to load the 'fitness' library on application startup.
-        init {
-            System.loadLibrary("fitness")
-        }
     }
 }
